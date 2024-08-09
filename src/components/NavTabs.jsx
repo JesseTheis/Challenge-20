24a1bb660a7954c60a19306f8d2c421fe6eb1cfe
Jesse Theis <jessetheis12@gmail.com>
2024-08-08 21:50:15 -0500

    renamed framework

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,4 +1,4 @@
-// TODO: Add a comment explaining how we are able to extract the key value pairs from props
+
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
@@ -7,7 +7,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#home"
           onClick={() => handlePageChange('Home')}
-          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+       
 
           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
         >
@@ -18,7 +18,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#about"
           onClick={() => handlePageChange('About')}
-          //  TODO: Add a comment explaining what this logic is doing
+          
 
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
@@ -29,7 +29,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#blog"
           onClick={() => handlePageChange('Blog')}
-          //  TODO: Add a comment explaining what this logic is doing
+          
 
           className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
         >
@@ -39,7 +39,7 @@ function NavTabs({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#contact"
-          //  TODO: Add a comment explaining what this logic is doing
+        
 
           onClick={() => handlePageChange('Contact')}
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
